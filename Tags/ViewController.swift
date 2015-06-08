8c098e15e175989ecf9a664c301b08f53f1ac3f4
Developer Inspirus <duc@developerinspirus.com>
2015-06-08 19:18:00 -0400

    Initial Commit

---
+++ Tags/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tags
+//
+//  Created by Duc Tran on 6/8/15.
+//  Copyright (c) 2015 Developer Inspirus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
